3744391a1f1deba99c5cb1deae2d089da3a515a9
aamir <16462830+batakpout@users.noreply.github.com>
2022-11-30 13:23:13 +0530

    Update VowelFrequency.scala

--- src/main/scala/md_algo/VowelFrequency.scala
+++ src/main/scala/md_algo/VowelFrequency.scala
@@ -3,22 +3,17 @@ package example
 object Main extends App {
 
    def challengeFunction(i: Int, s: String): String = {
-
-    val f = (c: Char) => "AEIOUaeiou".indexOf(c) == -1
     type S = String
     val o = i.toString.reverse.toInt
     def p(n: Int, s: S, r: S = ""): S = {
-      if (s.isEmpty) r
+      if (s == "") r
       else {
         val h = s(0)
-        if (f(h)) p(n, s.tail, r + h)
+        if ("AEIOUaeiou".indexOf(h) == -1) p(n, s.tail, r + h)
         else {
-          val m = n % 10
-          val z = s.span(x => x.toLower == h.toLower)
-          val j = r + h.toString * m
-          if (n / 10 == 0)
-            p(o, z._2, j)
-          else p(n / 10, z._2, j)
+          val z = s.span(_.toLower == h.toLower)
+          val j = r + h.toString * (n % 10)
+          if (n / 10 == 0) p(o, z._2, j) else p(n / 10, z._2, j)
         }
       }
     }
